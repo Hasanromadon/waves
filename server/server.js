70ad1bbf49cb419e04d088e8f41f26707be725a3
hasan <hsanromadon@gmail.com>
2022-01-11 22:50:55 +0700

    final

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const mongoose = require('mongoose');
@@ -40,7 +41,6 @@ app.use((err, req, res) => {
 
 app.use(express.static('client/build'));
 if (process.env.NODE_ENV === 'production') {
-  const path = require('path');
   app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
