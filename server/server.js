77d9b73e434ef4064245db6cc918ddf35af00e6e
hasan <hsanromadon@gmail.com>
2022-01-12 08:26:08 +0700

    adding final project

--- server/server.js
+++ server/server.js
@@ -39,12 +39,10 @@ app.use((err, req, res) => {
   handleError(err, res);
 });
 
+app.use(express.static('client/build'));
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use('/', express.static('build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
 }
 
