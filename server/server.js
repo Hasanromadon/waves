4c66a2acebfefc64802a10435d178509d8874c34
hasan <hsanromadon@gmail.com>
2022-01-12 07:52:46 +0700

    final

--- server/server.js
+++ server/server.js
@@ -45,6 +45,12 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
 }
+app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
